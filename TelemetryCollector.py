77fc1a8df897fc33642f4890e6b1b4311f10e3b3
Nunzio Barone <nunzio03@hotmail.it>
2019-06-24 18:36:14 +0200

    Update TelemetryCollector.py

--- TelemetryCollector.py
+++ TelemetryCollector.py
@@ -1,9 +1,21 @@
 import paho.mqtt.client as mqtt
 
+
 speed_index = 0
 position_index = 0
-position_TRIGGER = 0
+tOil_index = 0
+th20_index = 0
+rpm_index = 0
+vbatt_index = 0
+gear_index = 0
+
 speed_TRIGGER = 0
+position_TRIGGER = 0
+tOil_TRIGGER = 2
+th20_TRIGGER = 2
+rpm_TRIGGER = 5
+vbatt_TRIGGER = 10
+gear_TRIGGER = 6
 
 def on_message(client, userdata, msg):
     global speed_index
@@ -20,6 +32,34 @@ def on_message(client, userdata, msg):
         if(position_index > position_TRIGGER):
             position_index=0
             client_remote.publish( msg.topic, msg.payload.decode("utf-8"))
+            
+    if(msg.topic == "data/formatted/rpm"):
+            rpm_index = rpm_index + 1
+            if(rpm_index > rpm_TRIGGER):
+                rpm_index=0
+                client_remote.publish( msg.topic, msg.payload.decode("utf-8"))
+    if(msg.topic == "data/formatted/th20"):
+            th20_index = th20_index + 1
+            if(th20_index > th20_TRIGGER):
+                th20_index=0
+                client_remote.publish( msg.topic, msg.payload.decode("utf-8"))
+    if(msg.topic == "data/formatted/tOil"):
+            tOil_index = tOil_index + 1
+            if(tOil_index > tOil_TRIGGER):
+                tOil_index=0
+                client_remote.publish( msg.topic, msg.payload.decode("utf-8"))
+                
+    if(msg.topic == "data/formatted/vbatt"):
+            vbatt_index = vbatt_index + 1
+            if(vbatt_index > vbatt_TRIGGER):
+                vbatt_index=0
+                client_remote.publish( msg.topic, msg.payload.decode("utf-8"))
+    if(msg.topic == "data/formatted/gear"):
+            gear_index = gear_index + 1
+            if(gear_index > gear_TRIGGER):
+                gear_index=0
+                client_remote.publish( msg.topic, msg.payload.decode("utf-8"))
+                            
 # client_remote.publish( msg.topic, msg.payload.decode("utf-8"))
 
 
